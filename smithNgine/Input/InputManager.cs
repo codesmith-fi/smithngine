3efd837eb873f77576583a45d3b1fc667550e95b
Erno Pakarinen <erno@codesmith.fi>
2013-03-26 13:33:10 +0200

    InputManager provides mouse input states. Buttons, X, Y and scrollwheel. Also event for position changes

--- smithNgine/Input/InputManager.cs
+++ smithNgine/Input/InputManager.cs
@@ -14,6 +14,13 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Codesmith.SmithNgine.Input
 {    
+    public enum MouseButton
+    {
+        Left = 0,
+        Middle,
+        Right
+    }
+
     public class InputManager
     {
         #region Fields/Attributes
@@ -23,10 +30,28 @@ namespace Codesmith.SmithNgine.Input
         private List<GamePadState> gamepadStates = new List<GamePadState>(MaxPlayers);
         private List<KeyboardState> previousKeyboardStates = new List<KeyboardState>(MaxPlayers);
         private List<GamePadState> previousGamepadStates = new List<GamePadState>(MaxPlayers);
+        private MouseState mouseState;
+        private MouseState previousMouseState;
         #endregion
 
         #region Properties
+        public int ScrollWheelValue
+        {
+            get { return mouseState.ScrollWheelValue; }
+        }
+
+        public int MouseX
+        {
+            get { return mouseState.X; }
+        }
+
+        public int MouseY
+        {
+            get { return mouseState.Y; }
+        }
         #endregion
+
+        public event EventHandler<MousePositionEventArgs> MousePositionChanged;
 
         #region Constructors
         public InputManager()
@@ -36,6 +61,7 @@ namespace Codesmith.SmithNgine.Input
                 keyboardStates.Add(Keyboard.GetState((PlayerIndex)i));
                 gamepadStates.Add(GamePad.GetState((PlayerIndex)i));
             }
+            mouseState = Mouse.GetState();
         }
         #endregion 
 
@@ -45,12 +71,26 @@ namespace Codesmith.SmithNgine.Input
             // copy previous states so we know what is being done in current frame
             previousKeyboardStates = new List<KeyboardState>(keyboardStates);
             previousGamepadStates = new List<GamePadState>(gamepadStates);
+            previousMouseState = mouseState;
+            
+            // Do we need to inform any listener about mouse position changes
+            if (MousePositionChanged != null)
+            {
+                if (!(previousMouseState.X == mouseState.X && previousMouseState.Y == mouseState.Y))
+                {
+                    MousePositionEventArgs args = new MousePositionEventArgs(
+                        previousMouseState.X, previousMouseState.Y,
+                        mouseState.X, mouseState.Y);
+                    MousePositionChanged(this, args);
+                }
+            }
 
             for (int i = 0; i < MaxPlayers; i++)
             {
                 keyboardStates[i] = Keyboard.GetState((PlayerIndex)i);
                 gamepadStates[i] = GamePad.GetState((PlayerIndex)i);
             }
+            mouseState = Mouse.GetState();
         }
 
         public bool IsKeyPressed(Keys key, PlayerIndex? playerInControl, out PlayerIndex keySource)
@@ -76,7 +116,7 @@ namespace Codesmith.SmithNgine.Input
             }
         }
 
-        public bool IsButtonPressed(Buttons button, PlayerIndex? playerInControl, out PlayerIndex buttonSource)
+        public bool IsGamePadButtonPressed(Buttons button, PlayerIndex? playerInControl, out PlayerIndex buttonSource)
         {
             buttonSource = PlayerIndex.One;
             if (playerInControl.HasValue)
@@ -97,6 +137,47 @@ namespace Codesmith.SmithNgine.Input
                 }
                 return false;
             }
+        }
+
+        // Checks if mouse button was being pressed
+        // If param "checkAgainst" is set to ButtonState.Pressed checks if the
+        // button is being held down. 
+        public bool IsMouseButtonPressed(MouseButton button, ButtonState checkAgainst = ButtonState.Released )
+        {
+            // Left button was pressed after previous check
+            if (button == MouseButton.Left &&
+                previousMouseState.LeftButton == checkAgainst &&
+                mouseState.LeftButton == ButtonState.Pressed )
+            {
+                return true;
+            }
+            // Right button was pressed after previous check
+            else if (button == MouseButton.Right &&
+                previousMouseState.RightButton == checkAgainst &&
+                mouseState.RightButton == ButtonState.Pressed)
+            {
+                return true;
+            }
+            // Middle button was pressed
+            else if (button == MouseButton.Middle &&
+                previousMouseState.MiddleButton == checkAgainst &&
+                mouseState.MiddleButton == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Helper which checks if button is being pressed between consecutive checks
+        public bool IsMouseButtonHeld(MouseButton button)
+        {
+            return IsMouseButtonPressed(button, ButtonState.Pressed);
+        }
+
+        // Check if the mouse wheel was rotated
+        public bool IsMouseWheelRotated()
+        {
+            return (mouseState.ScrollWheelValue != previousMouseState.ScrollWheelValue);
         }
         #endregion 
     }
---
+++ smithNgine/Input/MousePositionEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Codesmith.SmithNgine.Input
+{
+    public class MousePositionEventArgs : EventArgs
+    {
+        public int X;
+        public int Y;
+        public int oldX;
+        public int oldY;
+
+        public MousePositionEventArgs(int oldX, int oldY, int newX, int newY)
+        {
+            this.X = newX;
+            this.Y = newY;
+            this.oldX = oldX;
+            this.oldY = oldY;
+        }
+
+        private MousePositionEventArgs() { }
+    }
+}
