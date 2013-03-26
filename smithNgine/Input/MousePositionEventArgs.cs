8e8c66be90f25ac462d6de926c09a365f667f3cb
Erno Pakarinen <erno@codesmith.fi>
2013-03-26 13:49:25 +0200

    MousePositionEventArgs contains Vector2 instead of ints for positions

--- smithNgine/Input/MousePositionEventArgs.cs
+++ smithNgine/Input/MousePositionEventArgs.cs
@@ -6,22 +6,19 @@
 // ***************************************************************************
 
 using System;
+using Microsoft.Xna.Framework;
 
 namespace Codesmith.SmithNgine.Input
 {
     public class MousePositionEventArgs : EventArgs
     {
-        public int X;
-        public int Y;
-        public int oldX;
-        public int oldY;
+        public Vector2 oldPosition;
+        public Vector2 newPosition;
 
         public MousePositionEventArgs(int oldX, int oldY, int newX, int newY)
         {
-            this.X = newX;
-            this.Y = newY;
-            this.oldX = oldX;
-            this.oldY = oldY;
+            oldPosition = new Vector2(oldX, newX);
+            newPosition = new Vector2(newX, newY);
         }
 
         private MousePositionEventArgs() { }
