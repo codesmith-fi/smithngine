021d73064ca9b969df83cdfb401c3540114e3760
Erno Pakarinen <erno@codesmith.fi>
2013-04-19 09:22:04 +0300

    Added Camera namespace and SimpleCamera and Camera classes

---
+++ smithNgine/Camera/Camera.cs
@@ -0,0 +1,111 @@
+﻿/**
+ * SmithNgine Game Framework
+ * 
+ * Copyright (C) 2013 by Erno Pakarinen / Codesmith (www.codesmith.fi)
+ * All Rights Reserved
+ */
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Codesmith.SmithNgine.Camera
+{
+    /// <summary>
+    /// Camera with Rotation, Scale and Parallax
+    /// </summary>
+    public class Camera : SimpleCamera
+    {
+        #region Fields
+        // Viewport rectangle
+        private Rectangle viewPortRect;
+        // View matrix 
+        private Matrix viewMatrix;
+        #endregion
+
+        #region Properties
+        public Vector2 Origin
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Zoom factor of the camera
+        /// <value>1.0 is normal zoom, smaller values zoom out</value>
+        /// </summary>
+        public float Zoom
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Rotation of the camera
+        /// <value>Angle in radians</value>
+        /// </summary>
+        public float Rotation
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Sets camera position and the viewport size
+        /// </summary>
+        /// <param name="position">Position of the camera</param>
+        /// <param name="viewPort">Viewport rectangle(size)</param>
+        public Camera(Vector2 position, Rectangle viewPort) 
+            : base(position)
+        {
+            // Set origin to the center of the viewport
+            Origin = new Vector2(viewPort.Width * 0.5f, viewPort.Height * 0.5f);
+            Zoom = 1.0f;
+            Rotation = 0.0f;
+            viewPortRect = viewPort;
+        }
+        #endregion
+
+        #region New methods
+        /// <summary>
+        /// Get view matrix for rendering 2d stuff
+        /// Origin of the translation is the center point of the viewport
+        /// </summary>
+        /// <param name="parallax">Parallax effect value</param>
+        /// <returns>2D view matrix</returns>
+        public Matrix GetViewMatrix(Vector2 parallax)
+        {
+            viewMatrix = Matrix.CreateTranslation(new Vector3(-Position * parallax, 0.0f)) *
+                   // Rotation translation
+                   Matrix.CreateRotationZ(Rotation) *
+                   // Scaling translation
+                   Matrix.CreateScale(Zoom, Zoom, 1) *
+                   // Translate around center of the viewport
+                   Matrix.CreateTranslation(
+                    new Vector3(Origin, 0.0f));
+            return viewMatrix;
+        }
+
+        /// <summary>
+        /// Get view matrix from the point of given origin. 
+        /// Rotation point is the given origin.
+        /// </summary>
+        /// <param name="parallax">Parallax effect value</param>
+        /// <param name="origin">Origin of translations</param>
+        /// <returns></returns>
+        public Matrix GetViewMatrix(Vector2 parallax, Vector2 origin)
+        {
+            viewMatrix = Matrix.CreateTranslation(
+                new Vector3(-Position * parallax, 0.0f)) *
+                    Matrix.CreateTranslation(new Vector3(-origin, 0.0f)) *
+                    // Rotation translation
+                    Matrix.CreateRotationZ(Rotation) *
+                    // Scaling translation
+                    Matrix.CreateScale(Zoom, Zoom, 1) *
+                    Matrix.CreateTranslation(new Vector3(origin, 0.0f));
+            return viewMatrix;
+        }
+        #endregion
+    }
+}
+
---
+++ smithNgine/Camera/SimpleCamera.cs
@@ -0,0 +1,109 @@
+﻿/**
+ * SmithNgine Game Framework
+ * 
+ * Copyright (C) 2013 by Erno Pakarinen / Codesmith (www.codesmith.fi)
+ * All Rights Reserved
+ */
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Codesmith.SmithNgine.Camera
+{
+    /// <summary>
+    /// Very basic camera, only position and movement
+    /// </summary>
+    public class SimpleCamera
+    {
+        #region Fields
+        // Position of the camera
+        private Vector2 position;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Get or set the position of the camera
+        /// <value>Position as a Vector2</value>
+        /// </summary>
+        public Vector2 Position 
+        { 
+            get { return position; }
+            set { position = value; }
+        }
+
+        /// <summary>
+        /// Get the X position of the camera
+        /// <value>X position as a float</value>
+        /// </summary>
+        public float X
+        {
+            get { return position.X; }
+            set { position.X = value; }
+        }
+
+        /// <summary>
+        /// Get the Y position of the camera
+        /// <value>Y position as a float</value>
+        /// </summary>
+        public float Y
+        {
+            get { return position.Y; }
+            set { position.Y = value; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Construct a camera with given position
+        /// </summary>
+        /// <param name="position">Position as a Vector2</param>
+        public SimpleCamera(Vector2 position)
+        {
+            Position = position;
+        }
+
+        /// <summary>
+        /// Construct a camera with position Vector2.Zero
+        /// </summary>
+        public SimpleCamera()
+            : this(Vector2.Zero)
+        {
+        }
+        #endregion
+
+        #region New methods
+        /// <summary>
+        /// Move the camera with given delta position
+        /// <example>
+        /// Move camera 10 units right
+        /// <code>
+        ///     myCamera.Move(new Vector2(10,0));
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="delta">Amount of units to move as a Vector2</param>
+        public virtual void Move(Vector2 delta)
+        {
+            position += delta;
+        }
+
+        /// <summary>
+        /// Move the camera on X axis
+        /// </summary>
+        /// <param name="deltaX">X coordinate delta as float</param>
+        public virtual void MoveX(float deltaX)
+        {
+            position.X += deltaX;
+        }
+
+        /// <summary>
+        /// Move the camera on Y axis
+        /// </summary>
+        /// <param name="deltaX">Y coordinate delta as float</param>
+        public virtual void MoveY(float deltaY)
+        {
+            position.Y += deltaY;
+        }
+        #endregion
+    }
+}
+
