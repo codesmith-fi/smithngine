eaa9232c68f09ef3064584060181ada03eeeeb56
Erno Pakarinen <erno@codesmith.fi>
2013-04-23 09:56:05 +0300

    Added MathConstants class

---
+++ smithNgine/MathUtil/MathConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codesmith.SmithNgine.MathUtil
+{
+    public static class MathConstants
+    {
+        const float PI = (float)Math.PI;
+        const float PIHalf = (float)Math.PI / 2f;
+        const float PIQuarter = (float)Math.PI / 4f;
+        const float PIDouble = (float)Math.PI * 2f;
+    }
+}
