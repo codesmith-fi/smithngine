0bbb13eb81a019237f3829791c4c96ddd972bd3e
Erno Pakarinen <erno@codesmith.fi>
2013-04-29 16:09:41 +0300

    Added Constant and RandomDepthGenerator

---
+++ smithNgine/Particles/Generators/ConstantDepthGenerator.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * SmithNgine Game Framework
+ * 
+ * Copyright (C) 2013 by Erno Pakarinen / Codesmith (www.codesmith.fi)
+ * All Rights Reserved
+ * 
+ * For licensing terms, see License.txt which reflects to the current license
+ * of this framework.
+ */
+namespace Codesmith.SmithNgine.Particles.Generators
+{
+    using System;
+    using Codesmith.SmithNgine.MathUtil;
+
+    [Serializable]
+    class ConstantDepthGenerator : ConstantPropertyGenerator
+    {
+        public ConstantDepthGenerator()
+        {
+        }
+
+        public ConstantDepthGenerator(float depth)
+            : base(depth)
+        {
+        }
+
+        public override void Apply(Particle p)
+        {
+            p.InitialDepth = Value;
+            p.Depth = p.InitialDepth;
+        }
+    }
+}
---
+++ smithNgine/Particles/Generators/RandomDepthGenerator.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * SmithNgine Game Framework
+ * 
+ * Copyright (C) 2013 by Erno Pakarinen / Codesmith (www.codesmith.fi)
+ * All Rights Reserved
+ * 
+ * For licensing terms, see License.txt which reflects to the current license
+ * of this framework.
+ */
+namespace Codesmith.SmithNgine.Particles.Generators
+{
+    using System;
+    using Codesmith.SmithNgine.MathUtil;
+
+    [Serializable]
+    public class RandomDepthGenerator : RangePropertyGenerator
+    {
+        public RandomDepthGenerator()
+        {
+        }
+
+        public RandomDepthGenerator(float start, float end, float variation)
+            : base(start, end, variation)
+        {
+        }
+
+        public override void Apply(Particle p)
+        {
+            p.InitialDepth = RandomValue;
+            p.Depth = p.InitialDepth;
+        }
+    }
+}
