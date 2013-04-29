102eca26e6716e4f9057bfce835f5b806051c36d
Erno Pakarinen <erno@codesmith.fi>
2013-04-29 16:08:24 +0300

    Refactored ParticlePool. Now there is a generic version of ObjectPool which is used by ParticlePool

---
+++ smithNgine/Datatypes/ObjectPool.cs
@@ -0,0 +1,157 @@
+﻿/**
+ * SmithNgine Game Framework
+ * 
+ * Copyright (C) 2013 by Erno Pakarinen / Codesmith (www.codesmith.fi)
+ * All Rights Reserved
+ * 
+ * For licensing terms, see License.txt which reflects to the current license
+ * of this framework.
+ */
+
+namespace Codesmith.SmithNgine.Datatypes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Tries to implement a pool for preallocated objects.
+    /// 
+    /// Pool has 
+    /// - maximum size (will not grow over this)
+    /// - GrowTreshold (more objects are added to the pool when there are 
+    ///   less objects than this in the pool)
+    /// - GrowAmount (how many objects are added when the pool needs to grow)
+    /// 
+    /// Use Get() to get a object out from the pool
+    /// Use Insert() to put a object back to the pool
+    /// Or you can also use the the property Object to get and set.
+    /// </summary>
+    public class ObjectPool<TObject> : IDisposable
+    {
+        #region Fields
+        // Pool grows by PoolGrowAmount if we are under this threshold
+        private readonly int PoolGrowTreshold;
+        // How much to grow when under the threshold
+        private readonly int PoolGrowAmount;
+        // The pool, FIFO queue.
+        private Queue<TObject> cache;
+        // Initial size of the cache
+        private int initialCacheSize;
+        // Maximum size limit, does not grow over this
+        private int maximumCache;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Get or set object in the pool
+        /// <value>The object to get or set</value>
+        /// </summary>
+        public TObject Object
+        {
+            get
+            {
+                return Get();
+            }
+
+            set
+            {
+                Insert(value);
+            }
+
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a cache for 100 Objects and maximum cache 
+        /// set to 1000 objects.
+        /// </summary>
+        public ObjectPool()
+            : this(100, 1000)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a cache for objects with initial amount
+        /// and maximum cache size.
+        /// </summary>
+        /// <param name="initialCache"></param>
+        /// <param name="maxLimit"></param>
+        public ObjectPool(int initialCache, int maxLimit)
+        {
+            Debug.Assert(initialCache <= maxLimit, "Initial pool size is bigger than maxLimit");
+            PoolGrowAmount = initialCache < 100 ? initialCache : 100;
+            PoolGrowTreshold = maxLimit < 1000 ? maxLimit : 1000;
+            initialCacheSize = initialCache;
+            maximumCache = maxLimit;
+            cache = new Queue<TObject>(initialCacheSize);
+            Grow(initialCacheSize);
+        }
+        #endregion
+
+        #region Public methods
+        /// <summary>
+        /// Put a object back to the cache if there is room
+        /// </summary>
+        /// <param name="p">Object to be pooled</param>
+        public void Insert(TObject p)
+        {
+            Debug.Assert(p != null, "Trying to add null object to ParticlePool");
+            if (cache.Count < maximumCache)
+            {
+                cache.Enqueue(p);
+            }
+        }
+
+        /// <summary>
+        /// Get a object from the pool
+        /// </summary>
+        /// <returns>The object</returns>
+        public TObject Get()
+        {
+            TObject p = default(TObject);
+            // Need to add more particles if we are under the treshold
+            if (cache.Count < PoolGrowTreshold)
+            {
+                Grow(PoolGrowAmount - cache.Count);
+            }
+
+            // Get a first free radical (err. object) from the pool 
+            p = cache.Dequeue();
+            Debug.Assert(p != null, "ObjectPool retrieved a null Object, thats certainly a free radical!:)");
+            return p;
+        }
+        #endregion
+
+        #region From IDisposable
+        /// <summary>
+        /// Disposes this
+        /// </summary>
+        public virtual void Dispose()
+        {
+            cache.Clear();
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Grow the cache by the given amount.
+        /// Uses Activator to create new instance of the templated object
+        /// using the default constructor.
+        /// </summary>
+        /// <param name="amount"></param>
+        private void Grow(int amount)
+        {
+            Debug.Assert(cache != null, "ObjectPool's queue is not instantiated!");
+            for (int i = 0; i < amount && cache.Count < maximumCache; ++i)
+            {
+                // Create new instance of the templated object with the default constructor
+                TObject obj = (TObject)Activator.CreateInstance(typeof(TObject));
+                Debug.Assert(obj != null, "Activator created a null instance of tempate object.");
+                cache.Enqueue(obj);
+            }
+        }
+        #endregion
+    }
+}
--- smithNgine/Particles/ParticlePool.cs
+++ smithNgine/Particles/ParticlePool.cs
@@ -14,6 +14,7 @@ namespace Codesmith.SmithNgine.Particles
     using System.Diagnostics;
     using System.Linq;
     using System.Text;
+    using Codesmith.SmithNgine.Datatypes;
 
     /// <summary>
     /// Tries to implement a pool for preallocated particles.
@@ -30,114 +31,18 @@ namespace Codesmith.SmithNgine.Particles
     /// Use Insert() to put a particle back to the pool
     /// Or you can also use the Particle property get/set.
     /// </summary>
-    public class ParticlePool : IDisposable
+    public class ParticlePool : ObjectPool<Particle>
     {
-        #region Fields
-        private const int PoolGrowTreshold = 100;
-        private const int PoolGrowAmount = 1000;
-        private Queue<Particle> cache;
-        private int initialCacheSize;
-        private int maximumCache;
-        #endregion
-
-        #region Properties
-        /// <summary>
-        /// Get or set a particle in the pool.
-        /// <value>Particle</value>
-        /// </summary>
-        public Particle Particle
-        {
-            get
-            {
-                return Get();
-            }
-
-            set
-            {
-                Insert(value);
-            }
-
-        }
-        #endregion
-
         #region Constructors
-        /// <summary>
-        /// Initializes a cache for 1000 Particles and maximum cache 
-        /// set to 10000 particles.
-        /// </summary>
-        public ParticlePool() : this(1000, 10000)
-        {
-        }
-
         /// <summary>
         /// Initializes a cache for Particles with initial amount
         /// and maximum cache size.
         /// </summary>
         /// <param name="initialCache"></param>
         /// <param name="maxLimit"></param>
-        public ParticlePool(int initialCache, int maxLimit = 10000)
+        public ParticlePool(int initialCache, int maxLimit)
+            : base(initialCache, maxLimit)
         {
-            initialCacheSize = initialCache;
-            maximumCache = maxLimit;
-            cache = new Queue<Particle>(initialCacheSize);
-            Grow(initialCacheSize);
-        }
-        #endregion
-
-        #region Public methods
-        /// <summary>
-        /// Put a particle back to the cache if there is room
-        /// Particle is set to defaults.
-        /// </summary>
-        /// <param name="p">A particle to be pooled</param>
-        public void Insert(Particle p)
-        {
-            Debug.Assert(p != null, "Trying to add null Particle to ParticlePool");
-            if (cache.Count < maximumCache)
-            {
-                p.Reset();
-                cache.Enqueue(p);
-            }
-        }
-
-        /// <summary>
-        /// Get a particle from the pool
-        /// </summary>
-        /// <returns>A particle</returns>
-        public Particle Get()
-        {
-            Particle p = null;
-            // Need to add more particles if we are under the treshold
-            if (cache.Count < PoolGrowTreshold)
-            {
-                Grow(PoolGrowAmount - cache.Count);
-            }
-
-            // Get a first free radical (err. particle) from the pool 
-            p = cache.Dequeue();
-            Debug.Assert(p != null, "ParticlePool retrieved a null Particle, thats certainly a free radical!:)");
-            return p;
-        }
-
-        #region From base classes
-        /// <summary>
-        /// Disposes this
-        /// </summary>
-        public void Dispose()
-        {
-            cache.Clear();
-        }
-        #endregion
-        #endregion
-
-        #region Private methods
-        private void Grow(int amount = PoolGrowAmount)
-        {
-            Debug.Assert(cache != null, "Particle List is null");
-            for (int i = 0; i < amount; ++i)
-            {
-                cache.Enqueue(new Particle(null));
-            }
         }
         #endregion
     }
